27e0cf248e85b1ced511c8352044bbbdd1a5f1ba
gacademyarmand <gacademyarmand@gmail.com>
2024-07-31 11:19:20 +0100

    click_listener

---
+++ app/src/main/java/android/example/com/androidlectures/MainActivity.kt
@@ -0,0 +1,29 @@
+package android.example.com.androidlectures
+
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+    }
+
+    fun clickHandler(view: View) {
+        // EditText nameEdittext = findViewById(R.id.etName)
+        var nameEditText : EditText = findViewById(R.id.etName)
+        var mainTextView : TextView = findViewById(R.id.tvMain)
+
+        var data = nameEditText.text.toString()
+        mainTextView.setText(data)
+
+    }
+}
