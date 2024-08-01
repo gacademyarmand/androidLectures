dafa715acb55aeba85997016e9cddf21f19d1b1b
gacademyarmand <gacademyarmand@gmail.com>
2024-08-01 14:02:08 +0200

    sms Broadcast Receiver: filter on message and phone number

--- app/src/main/java/android/example/com/androidlectures/SmsReceiver.kt
+++ app/src/main/java/android/example/com/androidlectures/SmsReceiver.kt
@@ -1,5 +1,6 @@
 package android.example.com.androidlectures
 
+import android.telephony.SmsMessage
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -9,6 +10,19 @@ class SmsReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         Log.i(TAG, "academy you have received an sms -- cognizant")
+        val bundle = intent.extras
+        if (bundle != null) {
+            val pdus = bundle["pdus"] as Array<Any>?
+            val messages: Array<SmsMessage?> = arrayOfNulls<SmsMessage>(pdus!!.size)
+            for (i in pdus!!.indices) {
+                messages[i] = SmsMessage.createFromPdu(pdus!![i] as ByteArray)
+            }
+            if (messages.size > -1) {
+                Log.i(TAG, "Message recieved: " + messages[0]?.getMessageBody())
+                Log.i(TAG, "Phone number is : " + messages[0]?.displayOriginatingAddress)
+
+            }
+        }
     }
 
     companion object{
