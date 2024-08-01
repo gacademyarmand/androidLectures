3081f021d686989ac4ab23cb77feffce3b82efae
gacademyarmand <gacademyarmand@gmail.com>
2024-08-01 12:13:15 +0200

    lifecycle activity

--- app/src/main/java/android/example/com/androidlectures/HomeActivity.java
+++ app/src/main/java/android/example/com/androidlectures/HomeActivity.java
@@ -1,6 +1,7 @@
 package android.example.com.androidlectures;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,10 +12,14 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class HomeActivity extends AppCompatActivity {
 
+    String TAG = HomeActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Log.i(TAG,"created --egg-- memory is being allocated");
 
         //getintent which started this activity
         String dataReceived = getIntent().getExtras().getString("mykey");
@@ -22,6 +27,36 @@ public class HomeActivity extends AppCompatActivity {
         //set it on a textview
         TextView homeView = findViewById(R.id.tvHome);
         homeView.setText(dataReceived);
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.w(TAG,"started --egg hatched -- ui is visible");
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(TAG,"wake up -- resume -foreground");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.v(TAG,"nap -- onpause-- background");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"hibernate-- onstop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG,"purged-- vanished-destroyed");
     }
 }
