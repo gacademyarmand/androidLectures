9ec741409cfcbc8b12d7e3b0aac003b9708b7a83
gacademyarmand <gacademyarmand@gmail.com>
2024-08-02 14:19:24 +0200

    send custom broadcast to second app

--- app/src/main/java/android/example/com/androidlectures/MainActivity.kt
+++ app/src/main/java/android/example/com/androidlectures/MainActivity.kt
@@ -1,16 +1,19 @@
 package android.example.com.androidlectures
 
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.provider.AlarmClock
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.app.NotificationCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -58,5 +61,38 @@ class MainActivity : AppCompatActivity() {
         var calIntent = Intent("cognizant.portugal.academy")
         startActivity(calIntent)
     }
+
+    fun sendFlightBroadcast(view: View) {
+        var flightIntent = Intent("ihave.flight")
+        sendBroadcast(flightIntent)
+    }
+
+    fun showNotification(view: View) {
+        val mNotifyButton: Button = findViewById(R.id.btnNotifyme)
+        lateinit var mNotifyManager: NotificationManager
+        val NOTIFICATION_ID = 0
+        val notificationIntent = Intent(
+            this,
+            MainActivity::class.java
+        )
+        val notificationPendingIntent = PendingIntent.getActivity(
+            this,
+            NOTIFICATION_ID, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+
+        mNotifyManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        val notifyBuilder = NotificationCompat.Builder(this, "channel_id")
+            .setContentTitle("You've been notified!")
+            .setContentText("This is your notification text.")
+            .setSmallIcon(R.drawable.ic_stat_name)
+            .setContentIntent(notificationPendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+        val myNotification = notifyBuilder.build()
+        mNotifyManager.notify(NOTIFICATION_ID, myNotification)
+    }
+
+
 
 }
